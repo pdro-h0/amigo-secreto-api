da14902fab6dbc0cdf1e5b94f2fafa5cbfe44227
pdro-h0 <pedro.pho792@gmail.com>
2024-05-23 22:50:45 -0300

    fix: ensure password is always the present day

--- src/services/auth.test.ts
+++ src/services/auth.test.ts
@@ -1,31 +1,34 @@
-import { describe, it, expect, beforeEach } from "vitest"
-import { InMemoryUserRepository } from "../repositories/in-memory/in-memory-user-repository"
-import { AuthService } from "./auth"
+import { describe, it, expect, beforeEach } from "vitest";
+import { InMemoryUserRepository } from "../repositories/in-memory/in-memory-user-repository";
+import { AuthService } from "./auth";
+import { getToday } from "../utils/get-today";
 
 let inMemoryUserRepository: InMemoryUserRepository;
-let authService:  AuthService;
+let authService: AuthService;
 
-describe("AUTH SERVICE", () =>{
-    beforeEach(() =>{
-        inMemoryUserRepository = new InMemoryUserRepository();
-        authService = new AuthService(inMemoryUserRepository);
-    });
+describe("AUTH SERVICE", () => {
+  beforeEach(() => {
+    inMemoryUserRepository = new InMemoryUserRepository();
+    authService = new AuthService(inMemoryUserRepository);
+  });
 
-    it("should be able to make password",  () =>{
-        const { password } = inMemoryUserRepository.create("15052024")
+  it("should be able to make password", () => {
+    const { password } = inMemoryUserRepository.create(
+      getToday().split("/").join("")
+    );
 
-        const valueOf = authService.validatePassword(password);
+    const valueOf = authService.validatePassword(password);
 
-        expect(valueOf).toEqual(true)
-    })
-    
-    it("should be able to authenticate",  () =>{
-        const { password } = inMemoryUserRepository.create("15052024");
+    expect(valueOf).toEqual(true);
+  });
 
-        const token  = authService.createToken(password)
+  it("should be able to authenticate", () => {
+    const { password } = inMemoryUserRepository.create("15052024");
 
-        const valueOf  = authService.validateToken(token, password);
+    const token = authService.createToken(password);
 
-        expect(valueOf).toEqual(true)
-    })
-})
+    const valueOf = authService.validateToken(token, password);
+
+    expect(valueOf).toEqual(true);
+  });
+});
